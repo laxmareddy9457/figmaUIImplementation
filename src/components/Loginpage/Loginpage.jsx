09b70215c05194f98a33dca17ee0c7977427c625
laxma.chelimala <@arcadis.com>
2025-11-12 14:43:11 +0530

    changes as per review

--- src/components/Loginpage/Loginpage.jsx
+++ src/components/Loginpage/Loginpage.jsx
@@ -3,7 +3,7 @@ import './Loginpage.css';
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
 
-import { FaEye, FaEyeSlash } from 'react-icons/fa';
+import { FaRegEye, FaRegEyeSlash } from 'react-icons/fa';
 
 const Loginpage = () => {
     const [showPassword, setShowPassword] = useState(false);
@@ -29,15 +29,18 @@ const Loginpage = () => {
                         <label className="inputLabel fontSmall" htmlFor="password">Password</label>
                         <section className='inputContainer'>
                             <input type={showPassword ? "text" : "password"} id="password" className="inputField fontSmall" placeholder="Enter Password" />
-                            {showPassword ? <FaEyeSlash className='cursor-pointer eyeicon' size={15} onClick={togglePasswordVisibility} /> : <FaEye className='cursor-pointer eyeicon' size={15} onClick={togglePasswordVisibility} />}
+                            {showPassword ? <FaRegEyeSlash className='cursor-pointer eyeicon' size={20} onClick={togglePasswordVisibility} /> : <FaRegEye className='cursor-pointer eyeicon' size={20} onClick={togglePasswordVisibility} />}
                         </section>
                     </div>
                     <div className="flex justify-end w-full">
-                        <Link to="/" className="text-sm transition-colors hover:text-blue-800 hover:underline max-sm:text-xs">Forgot Password?</Link>
+                        <Link to="/" className="text-sm transition-colors text-[#2563EB]  hover:underline max-sm:text-xs">Forgot Password?</Link>
                     </div>
-                    <button type="submit" className="loginBtn">
-                        Login</button>
-                    <Link to="/register" className="createAccountBtn">Create New Account</Link>
+                    <div className='flex flex-col gap-[24px] w-full'>
+                        <button type="submit" className="loginBtn">
+                            Login</button>
+                        <Link to="/register" className="createAccountBtn">Create New Account</Link>
+                    </div>
+
                 </div >
             </form >
         </div >
