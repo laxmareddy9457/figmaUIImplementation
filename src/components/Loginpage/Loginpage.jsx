fbff58278acaccaa9082a80710e37e7ea4b609ca
laxma.chelimala <@arcadis.com>
2025-11-12 16:08:13 +0530

    changes as per review

--- src/components/Loginpage/Loginpage.jsx
+++ src/components/Loginpage/Loginpage.jsx
@@ -18,22 +18,24 @@ const Loginpage = () => {
                 <h1 className="heading">DFW Customer Portal (DEV)</h1>
                 <div className="loginMainBody">
                     <h1 className="loginHeading fontSmall">Login into your Account</h1>
+                    <div className='flex flex-col gap-[16px] w-full'>
+                        <div className="inputGroup">
+                            <label className="inputLabel fontSmall" htmlFor="email">Email</label>
+                            <section className='inputContainer'>
+                                <input type="email" id="email" className="text-[#687080] inputField fontSmall" placeholder="Enter Email" />
+                            </section>
+                        </div>
 
-                    <div className="inputGroup">
-                        <label className="inputLabel fontSmall" htmlFor="email">Email</label>
-                        <section className='inputContainer'>
-                            <input type="email" id="email" className="inputField fontSmall" placeholder="Enter your username" />
-                        </section>
-                    </div>
-                    <div className="inputGroup">
-                        <label className="inputLabel fontSmall" htmlFor="password">Password</label>
-                        <section className='inputContainer'>
-                            <input type={showPassword ? "text" : "password"} id="password" className="inputField fontSmall" placeholder="Enter Password" />
-                            {showPassword ? <FaRegEyeSlash className='cursor-pointer eyeicon' size={20} onClick={togglePasswordVisibility} /> : <FaRegEye className='cursor-pointer eyeicon' size={20} onClick={togglePasswordVisibility} />}
-                        </section>
-                    </div>
-                    <div className="flex justify-end w-full">
-                        <Link to="/" className="text-sm transition-colors text-[#2563EB] underline max-sm:text-xs">Forgot Password?</Link>
+                        <div className="inputGroup">
+                            <label className="inputLabel fontSmall" htmlFor="password">Password</label>
+                            <section className='inputContainer'>
+                                <input type={showPassword ? "text" : "password"} id="password" className="text-[#687080] inputField fontSmall" placeholder="Enter Password" />
+                                {showPassword ? <FaRegEyeSlash className='cursor-pointer eyeicon' size={20} color={'#374151'} onClick={togglePasswordVisibility} /> : <FaRegEye className='cursor-pointer eyeicon' color={'#374151'} size={20} onClick={togglePasswordVisibility} />}
+                            </section>
+                        </div>
+                        <div className="flex justify-end w-full">
+                            <Link to="/" className="text-[14px] transition-colors text-[#2563EB] text-w font-medium underline max-sm:text-xs">Forgot Password?</Link>
+                        </div>
                     </div>
                     <div className='flex flex-col gap-[24px] w-full'>
                         <button type="submit" className="loginBtn">
